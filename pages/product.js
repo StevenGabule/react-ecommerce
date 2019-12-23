690b529ccc8232ffa19aebbf4039f36633bdf763
John Paul Lim Gabule <lucas.gabule@gmail.com>
2019-12-24 06:17:28 +0800

    implement the showing specific products and add some changes in baseUrl for environment setup

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,22 @@
-function Product() {
-  return <>product</>;
+import Axios from "axios";
+import ProductSummary from "../components/Product/ProductSummary";
+import ProductAttributes from "../components/Product/ProductAttributes";
+import baseUrl from "../utils/baseUrl";
+
+function Product({ product }) {
+  return (
+    <>
+      <ProductSummary {...product} />
+      <ProductAttributes {...product} />
+    </>
+  );
 }
+
+Product.getInitialProps = async ({ query: { _id } }) => {
+  const url = `${baseUrl}/api/product`;
+  const payload = { params: { _id } };
+  const response = await Axios.get(url, payload);
+  return { product: response.data };
+};
 
 export default Product;
