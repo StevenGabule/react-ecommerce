0420448ac2b474ab529fce8b865d709d226659f1
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-11 02:34:04 +0800

    implement pagination in the frontpage

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,25 @@ import React from "react";
 import axios from "axios";
 import ProductList from "../components/Index/ProductList";
 import baseUrl from "../utils/baseUrl";
+import ProductPagination from "../components/Index/ProductPagination";
 
-function Home({ products }) {
-  return <ProductList products={products} />;
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
 
-Home.getInitialProps = async () => {
-  // fetch data from server
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
-  return { products: response.data };
+  const payload = { params: { page, size } };
+  // fetch data from server
+  const response = await axios.get(url, payload);
+  return response.data;
   // return response data as an object
   // NOTE: this object will be merged with existing props
 };
