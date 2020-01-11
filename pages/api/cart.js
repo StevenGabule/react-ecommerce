8e86ae3f717601d16e7f6ed21fc2d64054538a0e
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-11 18:53:35 +0800

    do some changes in the cartjs api to solve in cart page

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import Cart from "../../models/Cart";
-import connectDB from "../../utils/connectDb";
+import connectDb from "../../utils/connectDb";
 
-connectDB();
+connectDb();
 
 const { ObjectId } = mongoose.Types;
 
@@ -12,11 +12,11 @@ export default async (req, res) => {
     case "GET":
       await handleGetRequest(req, res);
       break;
-    case "DELETE":
-      await handleDeleteRequest(req, res);
-      break;
     case "PUT":
       await handlePutRequest(req, res);
+      break;
+    case "DELETE":
+      await handleDeleteRequest(req, res);
       break;
     default:
       res.status(405).send(`Method ${req.method} not allowed`);
@@ -28,13 +28,11 @@ async function handleGetRequest(req, res) {
   if (!("authorization" in req.headers)) {
     return res.status(401).send("No authorization token");
   }
-
   try {
-    let userId;
-    ({ userId } = jwt.verify(
+    const { userId } = jwt.verify(
       req.headers.authorization,
       process.env.JWT_SECRET
-    ));
+    );
     const cart = await Cart.findOne({ user: userId }).populate({
       path: "products.product",
       model: "Product"
@@ -42,7 +40,7 @@ async function handleGetRequest(req, res) {
     res.status(200).json(cart.products);
   } catch (error) {
     console.error(error);
-    res.status(403).send("Please login again!");
+    res.status(403).send("Please login again");
   }
 }
 
@@ -56,20 +54,20 @@ async function handlePutRequest(req, res) {
       req.headers.authorization,
       process.env.JWT_SECRET
     );
-    // get user cart based on userID
+    // Get user cart based on userId
     const cart = await Cart.findOne({ user: userId });
-    // check if product already exists in cart
+    // Check if product already exists in cart
     const productExists = cart.products.some(doc =>
       ObjectId(productId).equals(doc.product)
     );
-    // if so, increment quantity (by provided to request)
+    // If so, increment quantity (by number provided to request)
     if (productExists) {
       await Cart.findOneAndUpdate(
         { _id: cart._id, "products.product": productId },
         { $inc: { "products.$.quantity": quantity } }
       );
     } else {
-      // if not, add new product with given quantity
+      // If not, add new product with given quantity
       const newProduct = { quantity, product: productId };
       await Cart.findOneAndUpdate(
         { _id: cart._id },
@@ -79,7 +77,7 @@ async function handlePutRequest(req, res) {
     res.status(200).send("Cart updated");
   } catch (error) {
     console.error(error);
-    res.status(403).send("Please login again!");
+    res.status(403).send("Please login again");
   }
 }
 
@@ -104,6 +102,6 @@ async function handleDeleteRequest(req, res) {
     res.status(200).json(cart.products);
   } catch (error) {
     console.error(error);
-    res.status(403).send("Please login again!");
+    res.status(403).send("Please login again");
   }
 }
