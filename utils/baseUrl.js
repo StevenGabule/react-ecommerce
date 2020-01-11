bb50cd47e69aaea07636e3130ec72e01cfe84ff7
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-11 18:30:45 +0800

    do some

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://reactreserve.lucasgabule.now.sh"
+    ? "https://reactreserve-hh196uap4.now.sh"
     : "http://localhost:3000";
 export default baseUrl;
