59b4825ac5e8b19d3b493be444250509db8132f2
John Paul Lim Gabule <lucas.gabule@gmail.com>
2023-03-17 06:40:02 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV:
-      "mongodb://lucasgabule:FacebookLover24@cluster0-shard-00-00-crjhu.mongodb.net:27017,cluster0-shard-00-01-crjhu.mongodb.net:27017,cluster0-shard-00-02-crjhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+      "mongodb://:@cluster0-shard-00-00-crjhu.mongodb.net:27017,cluster0-shard-00-01-crjhu.mongodb.net:27017,cluster0-shard-00-02-crjhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
     JWT_SECRET: "nadadnadkanduandasdncd",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/johnlook/image/upload",
     CLOUDINARY_API_KEY: "162639725592421",
